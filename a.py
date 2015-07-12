f732670ec82df3e5087e736125cc7f7ba615cc0e
harshil goel <harshil158@gmail.com>
2015-07-12 22:20:37 +0530

    gets mine credentials

--- a.py
+++ a.py
@@ -1,4 +1,6 @@
 from github import Github
+import requests
+import json
 
 user=raw_input()
 password=raw_input()
@@ -7,8 +9,27 @@ g=Github(user,password)
 
 a=g.get_user().name
 
+k=0
+
 for repo in g.get_user().get_repos():
+    k+=1
+    if k==1:
+        continue
+    owner=""
     if repo.parent:
-        print repo.name,repo.parent.owner.login
+        owner=repo.parent.owner.login
     else: 
-        print repo.name,repo.owner.login
+        owner=repo.owner.login
+    url="https://api.github.com/repos/"+owner+"/"+repo.name+"/stats/contributors"
+    print repo.name,owner,url,
+    r=requests.get(url)
+    repoItem=json.loads(r.text or r.content)
+
+    for i in repoItem:
+        if i["author"]["login"]!="darkcoderrises":
+            continue
+        print i["author"]["login"],
+        k=0
+        for j in  i["weeks"]:
+            k+=int(j["a"])+int(j["c"])
+        print k
