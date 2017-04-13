891bdba10f49dbb189c788c2f69bb55bf8cf792b
Harshil Goel <darkcoderrises@users.noreply.github.com>
2017-04-13 23:17:02 +0530

    Formatting little bit

--- a.py
+++ a.py
@@ -2,39 +2,38 @@ from github import Github
 import requests
 import json
 
-User=raw_input()
-password=raw_input()
+user_name = raw_input()
+password = raw_input()
 
-g=Github(User,password)
+g = Github(user_name,password)
 
-user=raw_input()
+user = raw_input()
 
-a=g.get_user(user).name
+a = g.get_user(user).name
 
 for repo in g.get_user(user).get_repos():
-    owner=""
-    if repo.parent:
-        owner=repo.parent.owner.login
-    else: 
-        owner=repo.owner.login
+    owner= repo.parent.owner.login if repo.parent else repo.owner.login
+    
     url="https://api.github.com/repos/"+owner+"/"+repo.name+"/stats/contributors"
-    print repo.name,owner,url,
-    r=requests.get(url)
-    repoItem=json.loads(r.text or r.content)
+    print repo.name, owner, url,
+    
+    r = requests.get(url)
+    repoItem = json.loads(r.text or r.content)
 
-    url1="https://api.github.com/repos/"+owner+"/"+repo.name+"/languages"
-    r1 = requests.get(url1)
-    repoItem1 = json.loads(r1.text or r1.content)
-    total=0
-    for i in repoItem1:
-        total += int(repoItem1[i])
+    contributionsUrl = "https://api.github.com/repos/"+owner+"/"+repo.name+"/languages"
+    req_contri = requests.get(contributionsUrl)
+    
+    repoContributions = json.loads(req_contri.text or req_contri.content)
+    repoContributions = [int(repoContributions[i]) for i in repoItem]
+    total = sum(repoContributions)
 
     for i in repoItem:
         if i["author"]["login"]!=user:
             continue
         print i["author"]["login"],
+        
         k=0
-        for j in  i["weeks"]:
+        for j in i["weeks"]:
             k+=int(j["a"])+int(j["c"])
-        print k,total,k*1.0/total*1.0
+        print k, total, float(k)/total
 
